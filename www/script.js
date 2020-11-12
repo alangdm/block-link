0e27e2144ab90e9c8520457dca6dc1f74bd47a5d
Alan Dávalos <alandavalosm@gmail.com>
2020-11-12 07:43:13 +0000

    deploy Thu Nov 12 2020 07:43:13 GMT+0000 (Coordinated Universal Time)

---
+++ www/script.js
@@ -0,0 +1,7 @@
+const hash = document.querySelector("#hash");
+window.addEventListener("hashchange", () => {
+  hash.textContent = location.hash;
+});
+const code = document.querySelector("code");
+const mainDemo = document.querySelector("block-link.card");
+code.textContent = mainDemo.outerHTML;
